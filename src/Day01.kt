0a130fec22e8994556c504e3ce5e08c04c362bbf
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2022-12-01 11:02:45 +0100

    Day 1 initial

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,24 @@
 fun main() {
+
+    fun sumCaloriesByElf(input: List<String>) =
+        input.split(String::isEmpty).map { it.sumOf(String::toInt) }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return sumCaloriesByElf(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return sumCaloriesByElf(input).sorted().reversed().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
+    check(part1(input) == 67622)
+    check(part2(input) == 201491)
     println(part1(input))
     println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -14,3 +14,22 @@ fun readInput(name: String) = File("src", "$name.txt")
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+
+/**
+ * Splits a list by the given predicate
+ *
+ * @param predicate function that takes a element of the list and return true when predicate is matched
+ *
+ * @return List of List with a split parts
+ */
+fun <A> List<A>.split(predicate: (A) -> Boolean): List<List<A>> {
+    fun go(input: List<A>): List<List<A>> {
+        val first = input.takeWhile { !predicate(it) }
+        val other = input.dropWhile { !predicate(it) }.drop(1)
+        when {
+            other.isEmpty() -> return listOf(first)
+            else -> return listOf(first) + go(other)
+        }
+    }
+    return go(this)
+}
