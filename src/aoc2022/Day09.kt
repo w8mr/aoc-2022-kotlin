e7505f380ea654c341f54061547b421cc3488b35
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2022-12-10 09:38:48 +0100

    Day 9 part 2

--- src/aoc2022/Day09.kt
+++ src/aoc2022/Day09.kt
@@ -20,12 +20,16 @@ fun main() {
     val move = seq(OneOf(up, down, left, right)+" ", number()+"\n") { d, c -> Move(d, c) }
     val parser = zeroOrMore(move)
 
-    fun part1(input: String): Int {
-        data class State(val hits: Set<Coord> = mutableSetOf(), val head: Coord = Coord(), val tail: Coord = Coord())
-        val moves = parser.parse(input)
-        println(moves)
+    fun solve(input: String, n: Int): Int {
+        data class State(
+            val hits: Set<Coord> = mutableSetOf(),
+            val head: Coord = Coord(),
+            val tails: List<Coord>
+        )
 
-        fun Coord.move(dir: Dir) : Coord =
+        val moves = parser.parse(input)
+
+        fun Coord.move(dir: Dir): Coord =
             when (dir) {
                 Up -> Coord(this.x, this.y + 1)
                 Down -> Coord(this.x, this.y - 1)
@@ -34,27 +38,30 @@ fun main() {
             }
 
         fun Coord.follow(head: Coord): Coord =
-            if (((this.x-head.x).absoluteValue>1) || ((this.y-head.y).absoluteValue>1)) {
-                Coord(this.x-(this.x-head.x).sign, this.y-(this.y-head.y).sign)
+            if (((this.x - head.x).absoluteValue > 1) || ((this.y - head.y).absoluteValue > 1)) {
+                Coord(this.x - (this.x - head.x).sign, this.y - (this.y - head.y).sign)
             } else {
                 this
             }
 
-        val r =  moves.fold(State()) { s, m ->
+        val r = moves.fold(State(tails = MutableList(n) { Coord() })) { s, m ->
             (0 until m.count).fold(s) { acc, _ ->
                 val newHead = acc.head.move(m.dir)
-                val newTail = acc.tail.follow(newHead)
-               // println("head: $newHead, tail: $newTail")
-                val newHits = acc.hits.plus(newTail)
-                State(newHits, newHead, newTail)
+                val newTails = acc.tails.scan(newHead) { previous, tail -> tail.follow(previous) }.drop(1)
+                val newHits = acc.hits.plus(newTails.last())
+                State(newHits, newHead, newTails)
             }
 
         }
         return r.hits.size
     }
 
+    fun part1(input: String): Int {
+        return solve(input, 1)
+    }
+
     fun part2(input: String): Int {
-        return TODO()
+        return solve(input, 9)
     }
     // test if implementation meets criteria from the description, like:/
     val testInput = readFile(2022, 9, 1).readText()
@@ -64,10 +71,11 @@ fun main() {
     println(part1(input))
     check(part1(input) == 6269)
 
-    check(part2(testInput) == TODO())
+    check(part2(testInput) == 1)
+    check(part2(readFile(2022, 9, 2).readText()) == 36)
 
     println(part2(input))
-    check(part2(input) == TODO())
+    check(part2(input) == 2557)
 
 }
 
