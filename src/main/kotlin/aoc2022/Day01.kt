c5daafb9bc078d73042f587914952648f168e879
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2023-01-11 19:32:00 +0100

    Cleanup Day 1 Use parser invoke

--- src/main/kotlin/aoc2022/Day01.kt
+++ src/main/kotlin/aoc2022/Day01.kt
@@ -5,18 +5,18 @@ import aoc.*
 class Day01() {
 
     val elf = number() sepBy "\n"
-    val elfs = elf sepBy "\n"
+    val parser = elf sepBy "\n"
 
     fun sumCaloriesByElf(input: List<List<Int>>) =
         input.map(List<Int>::sum)
 
     fun part1(input: String): Int {
-        val parsed = elfs.parse(input)
+        val parsed = parser(input)
         return sumCaloriesByElf(parsed).max()
     }
 
     fun part2(input: String): Int {
-        val parsed = elfs.parse(input)
+        val parsed = parser(input)
         return sumCaloriesByElf(parsed).sortedDescending().take(3).sum()
     }
 
