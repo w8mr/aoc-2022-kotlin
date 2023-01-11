be6faafae270cb2e8d396992e25d370a0936c783
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2023-01-11 23:25:19 +0100

    Day 4 IntRange

--- src/main/kotlin/aoc2022/Day04.kt
+++ src/main/kotlin/aoc2022/Day04.kt
@@ -1,34 +1,26 @@
 package aoc2022
 
-import aoc.parser.followedBy
-import aoc.parser.number
-import aoc.parser.seq
-import aoc.parser.zeroOrMore
+import aoc.parser.*
+import aoc.parser.Parsers.eol
+import aoc.parser.Parsers.number
 
 class Day04() {
-    data class Zone(val start: Int, val end: Int) {
-        operator fun contains(other: Zone): Boolean =
-            other.start >= this.start &&
-                    other.start <= this.end &&
-                    other.end <= this.end &&
-                    other.end >= this.start
+    operator fun IntRange.contains(other : IntRange) =
+        (other.start in this) && (other.endInclusive in this)
 
-        infix fun overlap(other: Zone): Boolean =
-            (other.start >= this.start && other.start <= this.end) ||
-                    (this.start >= other.start && this.start <= other.end)
+    infix fun IntRange.overlap(other : IntRange) =
+        (other.start in this) || (start in other)
 
-    }
-
-    val zone = seq(number() followedBy "-", number(), ::Zone)
-    val pair = seq(zone followedBy ",", zone followedBy "\n")
-    val pairs = zeroOrMore(pair)
+    val zone = seq(number and "-", number, ::IntRange)
+    val pair = seq(zone and ",", zone and eol)
+    val parser = oneOrMore(pair)
 
     fun part1(input: String): Int {
-        return pairs.parse(input).map { (e1, e2) -> e1 in e2 || e2 in e1 }.count { it }
+        return parser(input).map { (e1, e2) -> e1 in e2 || e2 in e1 }.count { it }
     }
 
     fun part2(input: String): Int {
-        return pairs.parse(input).map { (e1, e2) -> e1 overlap e2 }.count { it }
+        return parser(input).map { (e1, e2) -> e1 overlap e2 }.count { it }
     }
 }
 
--- src/main/kotlin/aoc2022/Readme.md
+++ src/main/kotlin/aoc2022/Readme.md
@@ -5,11 +5,12 @@ Puzzle type
 1 Collection operations
 2 Scoring
 3 Sets/intersect
+4 Overlapping ranges
 
 Learned about
 
 1 sortedDescending
 2 sumOf
 3 chunked
-
+4 IntRange
 
